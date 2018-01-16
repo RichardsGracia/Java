c1ed0c6c67474d7913e743e47dc7c163f0df23de
RichardsGracia <32587630+RichardsGracia@users.noreply.github.com>
2018-01-16 10:49:15 +0700

    Add files via upload

---
+++ SKPL_Richard_15111213_OOAD_FINAL/Kedua.java
@@ -0,0 +1,6 @@
+public class Kedua{
+public void setAaInA(String Aa){   
+Pertama a=new Pertama();
+a.setAa(Aa);
+}
+}
---
+++ SKPL_Richard_15111213_OOAD_FINAL/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(){
+Pertama a=new Pertama();
+Kedua b= new Kedua();
+b.setAaInPertama("Ini Pertama");
+System.out.print(a.getAa());
+    }
+}
